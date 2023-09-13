ad8ab578c71a0609abc8ef701f8ce490579f1a87
Adair <adairtrejomorales104@gmail.com>
2023-09-13 11:23:27 -0600

    commit from intellij

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -10,8 +10,10 @@ import java.time.LocalDateTime;
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findByActivoTrue(Pageable paginacion);
 
+    // primer cambio en intellij
+
     @Query("""
-            select m from Medico m where m.activo= true AND m.especialidad=:especialidad and m.id not in(select c.medico.id from Consulta c c.data=:fecha) order by rand() limit 1
+            select m from Medico m where m.activo= true AND m.especialidad=:especialidad and m.id not in(select c.medico.id from Consulta c where c.data=:fecha) order by rand() limit 1
             """)
     Medico seleccionarMedicoConEspecilidadEnFecha(Especialidad especialidad, LocalDateTime fecha);
 }
