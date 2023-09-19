3d7348e62413e70ab0c6ce78f906060631990a84
Adair <adairtrejomorales104@gmail.com>
2023-09-19 09:59:55 -0600

    commit update 3 from intellij documentacion

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,9 +1,10 @@
 package med.voll.api.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import med.voll.api.domain.consulta.AgendaDeConsultaService;
 import med.voll.api.domain.consulta.DatosAgendarConsultas;
-import med.voll.api.domain.consulta.DatosDetalleConsulta;
+import med.voll.api.infra.errores.ValidacionDeIntegridad;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/consultas")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultaController {
 
     @Autowired
@@ -18,7 +20,7 @@ public class ConsultaController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity agendar(@RequestBody @Valid DatosAgendarConsultas datos) {
+    public ResponseEntity agendar(@RequestBody @Valid DatosAgendarConsultas datos) throws ValidacionDeIntegridad {
         var response = service.agendar(datos);
         return ResponseEntity.ok(response);
     }
--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -30,6 +30,8 @@ public class SecurityConfigurations {
                 .permitAll()
                 .requestMatchers(HttpMethod.POST, "/medicos")
                 .permitAll()
+                .requestMatchers("/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**")
+                .permitAll()
                 .anyRequest().authenticated()
                 .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
